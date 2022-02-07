5aeb9d3e65d703bc0f1a48b8038d9667e289c16f
NarcelioMonte <narcelio@labsit.io>
2022-02-07 16:41:41 -0300

    Adicionada a Conexão com a Base de Dados

---
+++ back/src/ProAtividade.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ProAtividade.API.Models;
+
+namespace ProAtividade.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Atividade> Atividades { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+    }
+
+}
